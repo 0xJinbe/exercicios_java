0e820cf9ae8a84a363262e6ec2d7afe82376d8e6
claudio-germano <wantuir.germano@gmail.com>
2021-05-11 11:27:41 -0300

    ex4

---
+++ src/ex3.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex3 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int num, horas;
+		double valor, sal;
+		num = sc.nextInt();
+		horas = sc.nextInt();
+		valor = sc.nextDouble();
+
+		sal = horas * valor;
+		System.out.println("NUMER = " + num);
+		System.out.printf("SALARY = %.2f%n", sal);
+
+	}
+}
