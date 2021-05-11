7640db0a7f16d6a989cc4c7829f1e36e6ac4515b
claudio-germano <wantuir.germano@gmail.com>
2021-05-11 14:15:12 -0300

    estrutura sequencial

---
+++ src/ex10.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class ex10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		double item, qnt, preco, total;
+		
+		item = sc.nextDouble();
+		qnt = sc.nextDouble();
+		
+		if (item == 1) {
+			total = qnt * 4;
+		} else if (item ==2) {
+			total = qnt * 4.50;
+		} else if (item == 3) {
+			total = qnt * 5;
+		} else if (item == 4) {
+			total = qnt * 2;
+		} else {
+			total = qnt * 1.50;
+		}
+			
+		System.out.println("Total: R$ " + total);
+		
+		sc.close();
+		
+		
+	}
+}
