e2b8edb806df66bf45e48a34e282131a1e4ae842
claudio-germano <wantuir.germano@gmail.com>
2021-05-13 15:15:56 -0300

    exercicios c entities externas

---
+++ src/application/Program1.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rectangle;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Rectangle rect = new Rectangle();
+
+		System.out.println("Enter rectangle width and height: ");
+		rect.altura = sc.nextDouble();
+		rect.largura = sc.nextDouble();
+
+		System.out.printf("Area =  %.2f%n", rect.area());
+		System.out.printf("Perimetro =  %.2f%n", rect.perimetro());
+		System.out.printf("Diagonal = %.2f%n", rect.diagonal());
+
+		sc.close();
+	}
+
+}
